d989c403c9f0f315ed8c3bd2a48fa62283242a73
Jayminho <jayme.campos17@gmail.com>
2024-12-15 22:30:42 -0300

    funções para iniciar, pausar e encerrar contagem

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//
 const btnOptions = document.querySelectorAll('.buttons-options button');
+const btnStartPauseTimer = document.querySelector('#timer-count');
 
 btnOptions.forEach((btn, index) => {
     btn.addEventListener('click', function() {
@@ -11,4 +13,37 @@ function activeBtn(index) {
         btn.classList.remove('active');
     })
     btnOptions[index].classList.add('active')
+}
+
+
+//
+btnStartPauseTimer.addEventListener('click', () => {
+    btnStartPauseTimer.classList.toggle('active')
+    startPauseCounting();
+})
+
+let contagemEmSegundos = 5;
+let intervalId = null;
+
+
+const contagemRegressiva = () => {
+    if (contagemEmSegundos <= 0) {
+        stopCounting()
+        return;
+    }
+    console.log(contagemEmSegundos);
+    contagemEmSegundos -= 1;
+}
+
+function startPauseCounting() {
+    if (intervalId) {
+        stopCounting();
+        return;
+    }
+    intervalId = setInterval(contagemRegressiva, 1000);
+}
+
+function stopCounting() {
+    clearInterval(intervalId);
+    intervalId = null;
 }
